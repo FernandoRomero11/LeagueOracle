96c3fc4ddbe9090d63bc043c650cf94fde61863f
Fernando Romero (A189898) <a189898@at4wireless.com>
2021-04-12 14:43:56 +0200

    feat(back & front): Champions screen add

--- frontend/src/app/pages/champions/champions.component.ts
+++ frontend/src/app/pages/champions/champions.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ChampionsService } from 'src/app/services/champions.service';
 
 @Component({
   selector: 'app-champions',
   templateUrl: './champions.component.html'
 })
 export class ChampionsComponent implements OnInit {
+  champions = [];
 
-  constructor() { }
-
+  constructor(private championsService: ChampionsService) { }
+  
   ngOnInit(): void {
+    this.getChampions();
+  }
+
+  getChampions(){
+    let myChampions$ = this.championsService.getChampions();
+    myChampions$.subscribe(result => {
+      result.sort((a,b) => a.name < b.name ? -1 : 1)
+      this.champions = result;
+    })
   }
 
 }
---
+++ frontend/src/app/services/champions.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChampionsService {
+
+  PHP_API_SERVER='http://localhost';
+
+  constructor(private http: HttpClient) { }
+
+  getChampions(): Observable<any[]>{
+    return this.http.get<any[]>(`${this.PHP_API_SERVER}/backend/getChampions.php`)
+  }
+}
